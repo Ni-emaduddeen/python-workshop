3c7158eaeeb1c61ff2478a5482e65e34a9249ee9
Ni-emaduddeen <ug671437003@ftu.ac.th>
2026-08-27 10:48:42 +0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -12,3 +12,13 @@ elif operation == '/' :
     result = number1 / number2
 
 print(number1, operation, number2 ,"=", result)
+import random
+
+number = random.randint(0,10)
+guess = int(input("guess a number between 0-10 :"))
+
+while guess != number :
+    print("try again na")
+    guess = int(input("guess a number between 0-10 :"))
+
+print("Gooddd!!!!!")
